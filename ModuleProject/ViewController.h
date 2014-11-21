c7991fc655b5a2e35b97050ddb61ba153218bf2a
z415073783 <415073783@qq.com>
2014-11-21 19:14:17 +0800

    asd
    asd

---
+++ ModuleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleProject
+//
+//  Created by Zlm on 14-11-17.
+//  Copyright (c) 2014年 xmjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModuleProjectTests/ModuleProjectTests.m
@@ -0,0 +1,40 @@
+//
+//  ModuleProjectTests.m
+//  ModuleProjectTests
+//
+//  Created by Zlm on 14-11-17.
+//  Copyright (c) 2014年 xmjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ModuleProjectTests : XCTestCase
+
+@end
+
+@implementation ModuleProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
